59b1437da53c91aee745a9b72ceef18dbe2dddcf
TiagoGuiomar99 <78052646+TiagoGuiomar99@users.noreply.github.com>
2021-02-05 00:14:56 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+function popFunction(){
+    alert("Muito Obrigada! \nO seu donativo será depositado na conta da Máscara Negra.")
+}
+$(document).ready(function () {
+    $('#new1').hover(function () {
+        $('#new1-line').removeClass('not-active');
+        $('#new1-line').addClass('active-line');
+    }, function () {
+        $('#new1-line').removeClass('active-line');
+        $('#new1-line').addClass('not-active');
+    });
+});
+  
+$(document).ready(function () {
+    $('#new2').hover(function () {
+        $('#new2-line').removeClass('not-active');
+        $('#new2-line').addClass('active-line');
+    }, function () {
+        $('#new2-line').removeClass('active-line');
+        $('#new2-line').addClass('not-active');
+    });
+});
+  
+$(document).ready(function () {
+    $('#new3').hover(function () {
+        $('#new3-line').removeClass('not-active');
+        $('#new3-line').addClass('active-line');
+    }, function () {
+        $('#new3-line').removeClass('active-line');
+        $('#new3-line').addClass('not-active');
+    });
+});
+  
+$(document).ready(function () {
+    $('#new4').hover(function () {
+        $('#new4-line').removeClass('not-active');
+        $('#new4-line').addClass('active-line');
+    }, function () {
+        $('#new4-line').removeClass('active-line');
+        $('#new4-line').addClass('not-active');
+    });
+});
+  
